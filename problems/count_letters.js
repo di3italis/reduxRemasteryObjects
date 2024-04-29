806730dd5a3b112fcbb9665ad771ef037b38e110
takashi <takashi.sh@proton.me>
2024-04-29 00:19:57 -0700

    init commit

---
+++ problems/count_letters.js
@@ -0,0 +1,23 @@
+/*
+Given a string, write a function "countLetters" that returns the most
+common character within the sentence. If there are multiple characters
+that appear the most, return the lexicographically smallest one
+(e.g. if 'a' and 'b' are both the most common, return 'a' because it
+is closest to the beginning of the alphabet). Be sure to exclude spaces
+and punctuation.
+*/
+
+const countLetters = () => {
+    // your code here
+}
+
+
+
+// console.log(countLetters("The quick, brown fox jumped over the lazy dog.")); // e
+
+
+/*** Do not change the code after this line ***/
+
+module.exports = {
+	countLetters,
+};
